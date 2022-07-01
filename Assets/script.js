6cf20f79fd6c5af959fa3230465108e3d1de2a63
PeachDoors <taylorpterrill@gmail.com>
2022-06-30 18:20:11 -0600

    pseudo code

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,28 @@
+//1. prompt user for password criterea
+//   password length between 8 and 128
+//   include lowercase, uppercase, numeric, and special character?
+//2. validate input
+//   check character length
+//   at least one character type
+//3. generate password
+//4. display password
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const lowerCaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const upperCaseLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const randomNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const specialChar = ["!","#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~", "]"];
+
+
+//Generates and Displays Password
+function generatePassword() {
+  console.log("button has been clicked");
+
+  return "generated password";
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +35,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
